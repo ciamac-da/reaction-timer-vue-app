0f7a28d24cd32aa79fed2ee3688a3e1e891eee1b
ciamac-da <ciamac.davoudi@gmail.com>
2021-11-07 14:09:53 +0100

    Verified by Martian v1.0.2 Starting a new game

---
+++ src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="block"> Click Me </div>
+</template>
+
+<script>
+export default {
+    name: "Block",
+    props:["delay"]
+}
+</script>
+
+<style scoped>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: green;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
