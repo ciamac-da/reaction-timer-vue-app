dbf38a88f4fe664b7debabde72991514e5c4244a
Ciamac Davoudi <ciamac.davoudi@proximity.de>
2021-11-10 10:45:29 +0100

    Verified by Marti4n v1.0.5 adding some touches style

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,11 +1,32 @@
 <template>
-    <p>
-       Reaction Time was : {{score}} ms! 
-    </p>
+    <p> Reaction Time was : {{score}} ms! </p>
+    <p class="rank">{{rank}}</p>
 </template>
 
 <script>
 export default {
-    props: ["score"]
+    props: ["score"],
+    data() {
+    return {
+    rank: null
+    }
+    },
+    mounted() {
+        if(this.score < 250) {
+        this.rank = "WTF...Hot Fingers Reaction :)"
+        } else if(this.score < 400) {
+        this.rank = "Rapid Reflexes"
+        } else {
+        this.rank = "Hmmm was a normal reaction...!"
+        }
+    },
 }
 </script>
+
+<style scoped>
+.rank {
+ font-size: 1.4em;
+ color: green;
+ font-weight: bold;
+}
+</style>
